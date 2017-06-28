d2ac08cf3bb4a1ae84e43c3a8f3bf8b168e7a9a4
github username <github email address>
2017-06-28 14:45:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+}
+
+var blt = new Sandwich("white", ["bacon", "lettuce", "tomato", "mayo"], "rectangle");
+var reuben = new Sandwich("rye", ["corned beef", "sauerkraut", "swiss", "russian dressing"], "diagonal");
